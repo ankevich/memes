109d6dd4d78c1e02d5ff97819dce1a481d393204
Annie Bauer <overheads165@gmail.com>
2021-04-05 19:45:21 +0300

    showMemes function

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// - скрыть фиолетовое окно
+// - скрыть фиолетовое окно +
 // - выбрать рандомную картинку
 // - показать картинку
 
 const answerMeme = () => {
-  document.getElementById('window').style.display = 'none'
+  document.getElementById('window').hidden = true
+  document.getElementById('answer').hidden = false
+
+  const memes = document.getElementsByTagName('img')
+  const memesArray = Array.prototype.slice.call(memes)
+
+  const maxMemes = memes.length
+  const showIndex = Math.ceil(Math.random() * maxMemes) - 1
+
+  memesArray.map((meme, index) => {
+    if (index == showIndex) {
+      meme.hidden = false
+    } else {
+      meme.hidden = true
+    }
+  })
 }
